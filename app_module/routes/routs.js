ec04c977b6c2a92878b240d94603c1a0fb8091c1
Volodia <44103015+mdntsl@users.noreply.github.com>
2019-11-28 10:38:06 -0800

    Volodia
    Final commit

--- app_module/routes/routs.js
+++ app_module/routes/routs.js
@@ -3,11 +3,13 @@ const  router = express.Router();
 let handlers = require('../handers/handlers.js');
 let bodyParser =require('body-parser');
 
+
 let  urlencodedParser = bodyParser.urlencoded({extended : true});
 
 
-router.get('/' , (req,res) =>{
+router.get('/' , (req,res)=>{
     res.render('main', {Title:'Main page'});
-    
 });
+
+
 module.exports = router;
--- index.js
+++ index.js
@@ -3,11 +3,30 @@ let express = require('express');
 let app = express();
 let server = require('http').createServer(app);
 let routs = require( __dirname + '/app_module/routes/routs.js');
+let io = require('socket.io').listen(server);
 
 app.set('view engine', 'ejs');
 app.use('/parts',express.static('parts'));
 
-
 app.use('/', routs);
 
-app.listen('3000');
+let users = [];
+let connections = [];
+
+io.sockets.on('connection', (socket) => {
+console.log('++++');
+    connections.push(socket);
+    
+
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('----');
+    });
+    socket.on('send message', (data) => {
+        io.sockets.emit('post message',{msg:data.msg, name:data.name,});
+    });
+});
+
+
+server.listen('3000');  
+
